2423bef57a086cf7e3a32d9acdc7c9a0aae66c47
Kinrany <kinrany@yandex.com>
2015-11-28 19:53:55 +0300

    Раскидал классы по отдельным файлам

---
+++ Server/Matchmaking/GameModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlayerIO.GameLibrary;
+
+namespace Matchmaking {
+	public class GameModel {
+
+		/// <summary>
+		/// Останавливает игру.
+		/// </summary>
+		public void Stop() {
+			OnGameEnded();
+		}
+
+		/// <summary>
+		/// Создаёт новую игру для двух игроков.
+		/// </summary>
+		/// <param name="player1">Первый игрок.</param>
+		/// <param name="player2">Второй игрок.</param>
+		public GameModel(Player player1, Player player2) {
+			this.player1 = player1;
+			this.player2 = player2;
+		}
+
+		public delegate void GameEndDelegate();
+		public event GameEndDelegate OnGameEnded;
+
+		Player player1, player2;
+	}
+}
---
+++ Server/Matchmaking/Player.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlayerIO.GameLibrary;
+
+namespace Matchmaking {
+	public class Player : BasePlayer {
+
+		public void Initialize(Game game) {
+			if (this.game == null) {
+				this.game = game;
+			}
+			else {
+				throw new InvalidOperationException("Player already initialized");
+			}
+		}
+
+		/// <summary>
+		/// Посылает игроку список со всеми остальными игроками.
+		/// </summary>
+		public void SendPlayerList() {
+			game.Log("Sending player list to " + this.ConnectUserId + ".");
+			foreach (Player player in game.Players) {
+				if (player != this) {
+					this.Send("User joined", player.ConnectUserId);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Вызывает другого игрока на игру.
+		/// </summary>
+		/// <param name="other">Вызванный игрок.</param>
+		public void ChallengePlayer(Player other) {
+			game.Log("Player " + this.ConnectUserId + " challenged player " + other.ConnectUserId + ".");
+
+			if (other == this) {
+				this.Send("Denied", "You can't challenge yourself.");
+				return;
+			}
+			if (challenged != null) {
+				challenged.Send("Challenge revoked", this.ConnectUserId);
+			}
+			challenged = other;
+			other.Send("Challenged", this.ConnectUserId);
+
+			if (other.Challenged == this) {
+				GameModel model = new GameModel(this, other);
+				this.GameCreated(model);
+				other.GameCreated(model);
+
+				game.ScheduleCallback(delegate () {
+					this.model.Stop();
+				}, 1000);
+			}
+		}
+
+		/// <summary>
+		/// Играет ли игрок в данный момент.
+		/// </summary>
+		public bool IsPlaying {
+			get {
+				return (model != null);
+			}
+		}
+		/// <summary>
+		/// Игрок, вызванный на игру.
+		/// </summary>
+		public Player Challenged {
+			get {
+				return challenged;
+			}
+		}
+
+		public void GameCreated(GameModel model) {
+			game.Log("Player " + this.ConnectUserId + " started playing.");
+
+			this.model = model;
+			model.OnGameEnded += GameEnded;
+			this.Send("Game started");
+		}
+
+
+		private Game game = null;
+		private Player challenged = null;
+		private GameModel model = null;
+
+		private void GameEnded() {
+			game.Log("Player " + this.ConnectUserId + " stopped playing.");
+
+			model = null;
+			this.Send("Game ended");
+		}
+	}
+}
